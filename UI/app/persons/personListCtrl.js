8917276b6ed96bc1182d47e36c87196b83787cec
Dagmawi Tesfa <dagmawitesfa12@gmail.com>
2017-08-27 15:30:12 -0400

    added  angular controller and view

---
+++ UI/app/persons/personListCtrl.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("myApp")
+        .controller("PersonListCtrl",
+            ["personResource",
+                PersonListCtrl]);
+
+    function PersonListCtrl(personResource) {
+        var vm = this;
+
+        personResource.query(function (data) {
+            vm.persons = data;
+        });
+    }
+}());
